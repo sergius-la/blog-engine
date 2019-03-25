91b5e627d2da5f76c400617fc8280e594ecad88f
Sergey L.A <sergius.la@gmail.com>
2019-03-24 19:55:21 -0700

    first commit

---
+++ app/blogengine/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", posts_list, name="posts_list_url"),
+    path("post/<str:slug>/", post_deail, name="post_detail_url"),
+    path("tag/", tags_list, name="tags_list_url"),
+    path("tag/<str:slug>", tag_detail, name="tag_detail_url")
+]
