f08bc22d33ea7112a131a7a4f05dc63ec1937443
Admin <sergius.la@gmail.com>
2019-05-27 18:55:23 -0700

    lesson

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Post, Tag
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
+from django.shortcuts import redirect
+
+from .models import Post, Tag
 from .utility import ObjectDetailMixin
+from .forms import TagForm
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -12,10 +15,22 @@ class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = "blog/post_detail.html"
 
-
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, "blog/tag_create.html", context={"form":form})
+    
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, "blog/tag_create.html", context={"form":bound_form})
+
 
 def tags_list(request):
     tags = Tag.objects.all()
